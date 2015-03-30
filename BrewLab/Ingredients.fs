54306c371a260dddf79c676e1c3cf8795ff5b9d1
Steve Pemberton <sjpemberton09@gmail.com>
2015-03-30 06:45:20 +0100

    Added additional Recipe model types

--- BrewLab/Ingredients.fs
+++ BrewLab/Ingredients.fs
@@ -2,10 +2,24 @@
 
 open Units
 
-type hop = {Name:string; Weight:float<g>; Alpha:float<percentage>; }
-type adjunct = {Name:string; Weight:float<g>; Alpha:float<percentage>; }
+type HopType =
+|Pellet
+|Leaf
+|Extract
 
+type HopAddition =
+|Bittering
+|Flavour
+|Aroma
+|DryHop
+
+type hop = {Name:string; Weight:float<g>; Alpha:float<percentage>; Time:float; Type:HopType; }
+type adjunct = {Name:string; Weight:float<g>; Description:string }
+type grain = {Name:string; Weight:float<g>; Potential:float<pgp>; Colour:float<EBC>}
+type yeast = {Name:string; Attenuation:float<percentage>; TempRange: float<degC>*float<degC> }
+type water = {Name:string;} //chemical profile
 
 type Ingredient =
 | Hop of hop
 | Adjunct of adjunct
+| Grain of grain
---
+++ BrewLab/Recipe.fs
@@ -0,0 +1,15 @@
+﻿namespace Models
+
+open Units
+
+type Recipe = {
+    Grain: grain list
+    Hops: hop list
+    Adjuncts: adjunct list
+    Yeast: yeast
+    Name: string
+    Efficincy: float<percentage>
+    BoilLength:float//In minutes
+    MashLength:float
+
+}
