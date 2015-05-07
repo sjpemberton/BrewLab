21a2256d2c2aee01c2ea4fe88a60558013e51bfa
sjpemberton <sjpemberton09@gmail.com>
2015-05-07 16:53:23 +0100

    Added data service layer

--- BrewLab/RecipeViewModel.fs
+++ BrewLab/RecipeViewModel.fs
@@ -19,7 +19,7 @@ type RecipeViewModel(recipe) as this =
     let estimatedGravity = this.Factory.Backing(<@ this.Gravity @>, recipe.EstimatedOriginalGravity)
     let grain = ObservableCollection<GrainViewModel>()
     let hopAdditions = ObservableCollection<HopViewModel>()
-    let handleRefresh event = this.RefreshParts
+    //let handleRefresh event = this.RefreshParts
     
     let addIngredient = 
         function 
@@ -42,45 +42,27 @@ type RecipeViewModel(recipe) as this =
 
     let addMaltCommand = 
         this.Factory.CommandSync(fun p -> 
-            addIngredient <| Grain { Grain = this.Grains.[0]; Weight = 0.0<kg> })
-        //this.RefreshParts)
+            addIngredient <| Grain { Grain = this.Grains.[0]; Weight = 0.0<kg> }
+            this.RefreshParts)
 
     let removeMaltCommand = 
         this.Factory.CommandSyncParam(fun grainVm -> 
             this.Grain.Remove(grainVm) |> ignore
-            (//this.RefreshParts
-             grainVm :> IDisposable).Dispose())
+            this.RefreshParts
+            handleIngredientUpdate Events.FermentableChange
+            (grainVm :> IDisposable).Dispose())
     
     let addHopCommand = 
         this.Factory.CommandSync(fun p -> 
-            addIngredient <| Hop { Hop = this.Hops.[0]; Weight = 0.0<g>; Time = 0.0<minute>; Type = HopType.Leaf })
-        //this.RefreshParts)
+            addIngredient <| Hop { Hop = this.Hops.[0]; Weight = 0.0<g>; Time = 0.0<minute>; Type = HopType.Leaf }
+            this.RefreshParts)
 
     let removeHopCommand = 
         this.Factory.CommandSyncParam(fun hopVm -> 
             this.HopAdditions.Remove(hopVm) |> ignore
-            (//this.RefreshParts
-             hopVm :> IDisposable).Dispose())
-    
-    //Temp fixed list of grain
-    let grains = 
-        [ { Name = "Maris Otter"
-            Potential = 308.765<pgpkg>
-            Colour = 4.0<EBC> }
-          { Name = "Cara Amber"
-            Potential = 292.075<pgpkg>
-            Colour = 20.0<EBC> }
-          { Name = "Cara Pils"
-            Potential = 267.04<pgpkg>
-            Colour = 10.0<EBC> } ]
-    
-    let hops = 
-        [ { Name = "East Kent Goldings"
-            Alpha = 7.9<percentage> }
-          { Name = "Northen Brewer"
-            Alpha = 11.0<percentage> } ]
-    
-    let hopTypes = [ Leaf; Pellet; Extract ]
+            this.RefreshParts
+            handleIngredientUpdate Events.HopChange
+            (hopVm :> IDisposable).Dispose())
     
     do 
         base.BindEvent(Events.RecipeEvent.Instance.Event, 
@@ -88,20 +70,23 @@ type RecipeViewModel(recipe) as this =
                             | Events.FermentableChange | Events.HopChange -> true
                             | _ -> false)), handleIngredientUpdate)
     
+    member x.Grain : ObservableCollection<GrainViewModel> = grain
+    member x.HopAdditions : ObservableCollection<HopViewModel> = hopAdditions
+    
     member x.Gravity with get () = estimatedGravity.Value and set (v) = estimatedGravity.Value <- v
     member x.Volume with get () = volume.Value and set (v) = volume.Value <- v
     member x.Bitterness with get () = ibu.Value and set (v) = ibu.Value <- v
     member x.Efficiency with get () = efficiency.Value and set (v) = efficiency.Value <- v
 
-    member x.Grains : grain<kg> list = grains //TODO - Move to a standing data service
-    member x.Hops : hop<g> list = hops //TODO - Move to a standing data service
-    member x.HopTypes = hopTypes
+    member x.Grains : grain<kg> list = DataService.grains 
+    member x.Hops : hop<g> list = DataService.hops
+    member x.HopTypes = DataService.hopTypes
+
     member x.AddMaltCommand = addMaltCommand
     member x.AddHopCommand = addHopCommand
     member x.RemoveMaltCommand = removeMaltCommand
     member x.RemoveHopCommand = removeHopCommand
-    member x.Grain : ObservableCollection<GrainViewModel> = grain
-    member x.HopAdditions : ObservableCollection<HopViewModel> = hopAdditions
+
     member private x.RefreshParts = this.UpdateModelSnapshot()
 
     override x.UpdateModel recipe = 
